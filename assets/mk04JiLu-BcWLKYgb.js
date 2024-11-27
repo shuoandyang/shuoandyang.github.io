2c9a693a89adc34be1e9d89e1c7ce4d0a41cf7aa
shuoandyang <111366780+shuoandyang@users.noreply.github.com>
2024-11-28 02:05:23 +0800

    Add files via upload

---
+++ assets/mk04JiLu-BcWLKYgb.js
@@ -0,0 +1,1 @@
+import{B as a,e as n,p as o,f as s}from"./index-Bx3SwnAl.js";const f=a("mk04",()=>{let t=n(1),u=o([{id:0,name:"返回主页",to:"/"},{id:1,name:"返回设置",to:"/sheZhi"}]);const i=s(()=>u.filter(e=>e.id<=t.value));function r(e){t.value=e}return{JiLuShuJuS:i,Gai:r}});export{f as u};
